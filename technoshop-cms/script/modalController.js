701fe8e99faaf1c8eedd11b768c8cac8aa67a095
Xenia Filippova <32355526+riva-sm@users.noreply.github.com>
2022-08-10 03:52:22 +0800

    реализовали модальное окно

---
+++ technoshop-cms/script/modalController.js
@@ -0,0 +1,21 @@
+// открытие модального окна
+const openModal = (modal, classOpen) => {
+  modal.classList.add(classOpen);
+};
+// закрытие модального окна
+const closeModal = (modal, classOpen) => {
+  modal.classList.remove(classOpen);
+};
+
+export const modalController = ({ modal, btn, classOpen, classClose }) => {
+  btn.addEventListener("click", () => {
+    // по нажатию на кнопку добавить товар открываем модальное окно
+    openModal(modal, classOpen);
+  });
+  // если клик мимо модального окна или по крестику, модальное окно закрывается
+  modal.addEventListener("click", ({ target }) => {
+    if (target === modal || target.classList.contains(classClose)) {
+      closeModal(modal, classOpen);
+    }
+  });
+};
